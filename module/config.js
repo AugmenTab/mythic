061aa63e4fe03164a4933e2967781ae8e7abeffa
AugmenTab <tbaum92@live.com>
2021-06-23 10:53:49 -0600

    Size dropdown now localized.

--- module/config.js
+++ module/config.js
@@ -15,30 +15,30 @@ mythic.ability = {
   description: "mythic.ability.description"
 }
 
-// mythic.size = {
-//   mini: "mythic.sizes.mini",
-//   small: "mythic.sizes.small",
-//   normal: "mythic.sizes.normal",
-//   large: "mythic.sizes.large",
-//   huge: "mythic.sizes.huge",
-//   hulking: "mythic.sizes.hulking",
-//   giant: "mythic.sizes.giant",
-//   immense: "mythic.sizes.immense",
-//   massive: "mythic.sizes.massive",
-//   great: "mythic.sizes.great",
-//   monumental: "mythics.size.monumental"
-// }
-
 mythic.size = {
-  mini: "Mini",
-  small: "Small",
-  normal: "Normal",
-  large: "Large",
-  huge: "Huge",
-  hulking: "Hulking",
-  giant: "Giant",
-  immense: "Immense",
-  massive: "Massive",
-  great: "Great",
-  monumental: "Monumental"
+  mini: "mythic.sizes.mini",
+  small: "mythic.sizes.small",
+  normal: "mythic.sizes.normal",
+  large: "mythic.sizes.large",
+  huge: "mythic.sizes.huge",
+  hulking: "mythic.sizes.hulking",
+  giant: "mythic.sizes.giant",
+  immense: "mythic.sizes.immense",
+  massive: "mythic.sizes.massive",
+  great: "mythic.sizes.great",
+  monumental: "mythic.sizes.monumental"
 }
+
+// mythic.size = {
+//   mini: "Mini",
+//   small: "Small",
+//   normal: "Normal",
+//   large: "Large",
+//   huge: "Huge",
+//   hulking: "Hulking",
+//   giant: "Giant",
+//   immense: "Immense",
+//   massive: "Massive",
+//   great: "Great",
+//   monumental: "Monumental"
+// }
