bb6d8eaf01bda68b81a4a939c1e9e9540efbcc93
AugmenTab <tbaum92@live.com>
2021-06-23 12:37:34 -0600

    Created skill sheet, needs stat checkboxes.

--- module/config.js
+++ module/config.js
@@ -15,6 +15,17 @@ mythic.abilitySheet = {
   description: "mythic.abilitySheet.description"
 }
 
+mythic.skillDifficulty = {
+  basic: "mythic.skillSheet.difficulties.basic",
+  adv: "mythic.skillSheet.difficulties.adv"
+}
+
+mythic.skillType = {
+  social: "mythic.skillSheet.types.social",
+  move: "mythic.skillSheet.types.move",
+  field: "mythic.skillSheet.types.field",
+}
+
 mythic.size = {
   mini: "mythic.sizes.mini",
   small: "mythic.sizes.small",
