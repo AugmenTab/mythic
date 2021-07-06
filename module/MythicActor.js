ac6513e6f60026c168676ba380bbcafec44761f0
AugmenTab <tbaum92@live.com>
2021-07-05 22:58:07 -0600

    Ability Pool updates when user changes field data.

--- module/MythicActor.js
+++ module/MythicActor.js
@@ -20,6 +20,17 @@ export class MythicActor extends Actor {
 
   _prepareNamedCharacterData(actorData) {
     if (actorData.type !== "Named Character") return;
-    console.log(actorData);
+    actorData.data.characteristics.extra.poolTotal = (
+      actorData.data.characteristics.str.abilityPool +
+      actorData.data.characteristics.tou.abilityPool +
+      actorData.data.characteristics.agi.abilityPool +
+      actorData.data.characteristics.wfr.abilityPool +
+      actorData.data.characteristics.wfm.abilityPool +
+      actorData.data.characteristics.int.abilityPool +
+      actorData.data.characteristics.per.abilityPool +
+      actorData.data.characteristics.cr.abilityPool +
+      actorData.data.characteristics.ch.abilityPool +
+      actorData.data.characteristics.ld.abilityPool
+    );
   }
 }
