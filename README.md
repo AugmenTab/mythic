681711c533e88da5199352fbad26cce2e0320e3f
AugmenTab <tbaum92@live.com>
2021-07-21 00:55:39 -0600

    Exp total & current now add input values.

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ An unofficial system implementation for playing the fan-made Halo: Mythic game o
 
 ## Known Bugs
 
-* None currently.
+* Talents tabs not accepting first attempt to access them - must click into Talents, then select Abilities & Traits, then select other tabs.
+* Talents tab not persisting previous selection when clicking to other tabs.
+* Experience apply buttons discarding first attempt to alter values, then work correctly afterward.
 
 ## Stretch Goals
 
--- module/sheets/MythicNamedCharacterSheet.js
+++ module/sheets/MythicNamedCharacterSheet.js
@@ -32,6 +32,29 @@ export default class MythicNamedCharacterSheet extends ActorSheet {
     super.activateListeners(html);
 
     html.find(".rollable").click(this._onRoll.bind(this));
+    html.find(".exp-total-apply").click(this._onExpApply.bind(this));
+    html.find(".exp-spent-apply").click(this._onExpApply.bind(this));
+  }
+
+  async _onExpApply(event) {
+    event.preventDefault();
+    const element = event.currentTarget;
+    let data = duplicate(this.actor.data);
+    let field;
+    if (element.classList[0] === "exp-total-apply") {
+      field = document.getElementById("exp-total-value");
+      const val = Math.floor(parseInt(field.value));
+      data.data.experience.total += (!isNaN(val)) ? val : 0;
+    } else if (element.classList[0] === "exp-spent-apply") {
+      field = document.getElementById("exp-spent-value");
+      const val = Math.floor(parseInt(field.value));
+      data.data.experience.spent += (!isNaN(val)) ? val : 0;
+    }
+    if (isNaN(parseInt(field.value))) {
+      ui.notifications.error("Invalid entry: please enter a whole number.")
+    }
+    field.value = "";
+    await this.actor.update(data);
   }
 
   async _onRoll(event) {
