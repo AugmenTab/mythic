a77be33a01b355b8ea3d12a2dd15be13a5c134c3
AugmenTab <tbaum92@live.com>
2021-08-03 21:22:18 -0600

    Added more _sortCombatants tiebreaker conditions.

--- module/MythicCombat.js
+++ module/MythicCombat.js
@@ -1,38 +1,61 @@
 /** @module MythicCombat */
 
+import { calculateCharacteristicModifier } from "./calculations.js";
+
 /**
- * 
- * @class
+ * Class representing the unique features of this system's Combat mechanics.
+ * @extends Combat
  */
 export default class MythicCombat extends Combat {
+
+  /**
+   * Sorts Combatants to determine their initiative order.
+   * @param {Combatant} a - The first Combatant to sort.
+   * @param {Combatant} b - The second Combatant to sort.
+   * @returns {number} The result of the comparison between the two Combatants.
+   */
   _sortCombatants(a, b) {
     const initA = Number.isNumeric(a.initiative) ? a.initiative : -9999;
     const initB = Number.isNumeric(b.initiative) ? b.initiative : -9999;
-    const difference = initB - initA;
-    if (difference !== 0) return difference;
+    const initDifference = initB - initA;
+    if (initDifference !== 0) return initDifference;
 
-    const agiA = (
-      a.actor.data.data.characteristics.agi.roll + 
+    const agilityA = a.actor.data.data.characteristics.agi.roll;
+    const agilityB = b.actor.data.data.characteristics.agi.roll;
+
+    const agiModA = (
+      calculateCharacteristicModifier(agilityA) + 
       a.actor.data.data.mythicCharacteristics.agi.total
     );
-    const agiB = (
-      b.actor.data.data.characteristics.agi.roll + 
+    const agiModB = (
+      calculateCharacteristicModifier(agilityB) + 
       b.actor.data.data.mythicCharacteristics.agi.total
     );
-    return agiB - agiA;
+    const modDifference = agiModB - agiModA;
+    if (modDifference !== 0) return modDifference;
+
+    return agilityB - agilityA;
   }
 
+  /**
+   * Advances the Combat to the next Turn.
+   * @returns {Promise.<Combat>} The updated Combat with the Turn advanced.
+   */
   async nextTurn() {
     const missing = this.combatants.filter(c => c.initiative === null);
     if (missing.length > 0) {
       missing.forEach(c => ui.notifications.error(
-        game.i18n.format("mythic."),
+        game.i18n.format("mythic.combat.error.missingInit"),
         { token: c.token.name }
       ));
       return this;
     }
   }
 
+  /**
+   * Advances the Combat to the next Round.
+   * @returns {Promise.<Combat>} The updated Combat with the Round advanced.
+   */
   async nextRound() {
     return this.update({ advanceTime: CONFIG.time.roundTime });
   }
