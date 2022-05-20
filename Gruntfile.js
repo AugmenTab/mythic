b9179e97d86b08cf68f82db62634b538a9afb6ab
AugmenTab <tbaum92@live.com>
2022-05-19 23:11:38 -0600

    Adds release task to Grunt file
    This adds a "release" task that will:
    
    * Compile Less files into a single minified CSS file
    * Copy all files that can't be minimized to an working directory
    * Uglifies and copies over all JavaScript files
    * Compresses the working directory into a zip file
    * Deletes the working directory
    
    This leaves only the system files in an appropriately-named zip
    file once the task is complete, which can be dropped immediately
    into GitHub as the assets for a new release.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,33 @@
 module.exports = function(grunt) {
   const DEV_PATH = "/home/foundry/foundrydata/Data/systems/mythic/";
+  const RELEASE_DIR = "/home/tab/Documents";
+  const RELEASE_PATH = RELEASE_DIR + "/mythic";
+  const RELEASE_TASKS = ["less", "copy:release", "uglify", "compress", "clean"];
 
   // Configuration
   grunt.initConfig({
+    // grunt-contrib-clean
+    clean: {
+      options: { force: true },
+      src: [ RELEASE_PATH ]
+    },
+
+    // grunt-contrib-compress
+    compress: {
+      main: {
+        options: {
+          archive: RELEASE_DIR + "/mythic.zip",
+          pretty: true
+        },
+        files: [{
+          expand: true,
+          cwd: RELEASE_PATH,
+          src: "**/*",
+          dest: "/"
+        }]
+      }
+    },
+
     // grunt-contrib-copy
     copy: {
       dev: {
@@ -14,6 +39,18 @@ module.exports = function(grunt) {
           {expand: true, src: "module/**",     dest: DEV_PATH},
           {expand: true, src: "templates/**",  dest: DEV_PATH},
           {expand: true, src: "mythic.js",     dest: DEV_PATH}
+        ]
+      },
+      release: {
+        files: [
+          {expand: true, src: "lang/*",        dest: RELEASE_PATH},
+          {expand: true, src: "templates/**",  dest: RELEASE_PATH},
+          {expand: true, src: "CHANGELOG.md",  dest: RELEASE_PATH},
+          {expand: true, src: "LICENSE.txt",   dest: RELEASE_PATH},
+          {expand: true, src: "mythic.css",    dest: RELEASE_PATH},
+          {expand: true, src: "README.md",     dest: RELEASE_PATH},
+          {expand: true, src: "system.json",   dest: RELEASE_PATH},
+          {expand: true, src: "template.json", dest: RELEASE_PATH}
         ]
       }
     },
@@ -31,6 +68,16 @@ module.exports = function(grunt) {
       }
     },
 
+    // grunt-contrib-uglify
+    uglify: {
+      main: {
+        files: [
+          {expand: true, src: "module/**/*.js", dest: RELEASE_PATH},
+          {expand: true, src: "mythic.js",      dest: RELEASE_PATH}
+        ]
+      }
+    },
+
     // grunt-contrib-watch
     watch: {
       peep: {
@@ -41,10 +88,14 @@ module.exports = function(grunt) {
   });
 
   // Load plugins
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-compress');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   // Tasks
   grunt.registerTask('default', ["less", "copy:dev"]);
+  grunt.registerTask('release', RELEASE_TASKS);
 };
